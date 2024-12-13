ed30c9e0ac6dc09134fd90c52a0b4540c1eb1ce3
mizanmahi <mizanmahi24@gmail.com>
2024-12-13 18:09:33 +0600

    add: cicd8

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(globalErrorHandler);
 app.use((req: Request, res: Response, next: NextFunction) => {
    res.status(httpStatus.NOT_FOUND).json({
       success: false,
-      message: "Oops! It looks like this page doesn't exist at all hahahaha.",
+      message: "Oops! It looks like this page doesn't exist at all.",
       error: {
          path: req.originalUrl,
          error: `The requested URL was not found on this server.`,
